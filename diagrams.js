385e2e84944fead757c03eefaeade34f40ad3aad
Henry Hirsch <henry@w3-net.de>
2016-05-18 14:47:18 +0200

    Change menu and title

--- diagrams.js
+++ diagrams.js
@@ -5,35 +5,31 @@ function getColData(col){
         if ($tds.text() != "") {
             resultCol.push($tds.text());
         }
-
     });
     return resultCol;
 };
 
-function drawExerciseChart() 
-{
-   // alert("test");
-        new Chartist.Line('#chart1', {
-            labels: getColData(1),
-            series: [
-                {
-                    name: 'Chinups',
-                    data: getColData(2)
-                },
-                {
-                    name: 'Pushups',
-                    data: getColData(3)
-                },
-                {
-                    name: 'Pullups',
-                    data: getColData(4)
-                },
-                {
-                    name: 'Dips',
-                    data: getColData(5)
-                }]
-        }, {
-            fullWidth: true
-        });
-
+function drawExerciseChart() {
+    new Chartist.Line('#chart1', {
+        labels: getColData(1),
+        series: [
+            {
+                name: 'Chinups',
+                data: getColData(2)
+            },
+            {
+                name: 'Pushups',
+                data: getColData(3)
+            },
+            {
+                name: 'Pullups',
+                data: getColData(4)
+            },
+            {
+                name: 'Dips',
+                data: getColData(5)
+            }]
+    }, {
+        fullWidth: true
+    });
 }
